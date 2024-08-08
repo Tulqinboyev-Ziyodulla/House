0862b66293ee59555cabf17a5e0dd1d81caee87c
Tulqinboyev-Ziyodulla <ziyodulla464@gmail.com>
2024-08-08 23:04:11 +0500

    incomplete

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import Header from './components/header/Header'
+import Hero from './components/hero/Hero'
+import Kategorey from './components/kategorey/Kategorey'
+import Footer from './components/footer/Footer'
+
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Kategorey/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
